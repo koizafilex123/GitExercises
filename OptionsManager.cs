aa1177a4650f2701680222ada0080a2d8c5fce4f
Barzo Jala <jalabarzo@mail.com>
2023-01-04 09:53:49 +0200

    added new project

---
+++ OptionsManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = {
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply",
+            "d - Divide",
+            "sabs - Substract Abs",
+        };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} x {b} = {a * b}");
+        }
+        public static void Divide(double a, double b)
+        {
+            Console.WriteLine($"{a} : {b} = {a / b}");
+        }
+        public static void SubstractAbs(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {Math.Abs(a - b)}");
+        }
+    }
+}
